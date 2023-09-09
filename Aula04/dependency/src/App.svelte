2576176c89f374c0ffb14a7f604e525218cdb94e
Pedro Henrique de Souza <phsouzaa2@gmail.com>
2023-09-08 22:17:20 -0300

    Adding new version to show selected country

--- Aula04/dependency/src/App.svelte
+++ Aula04/dependency/src/App.svelte
@@ -4,28 +4,16 @@
 	import CountryFlag from './CountryFlag.svelte';
   
 	let selectedCountry = '';
-	let independentCountries = [];
   
 	function handleCountrySelected(event) {
 	  selectedCountry = event.detail;
 	}
-  
-	function updateIndependentCountries(country) {
-	  if (country && !independentCountries.includes(country)) {
-		independentCountries = [...independentCountries, country];
-	  }
-	}
   </script>
   
-  <h1>Svelte Country Flag Viewer</h1>
+  <h1>Select Country Program</h1>
   <CountryFlag on:countrySelected={handleCountrySelected} />
   
-  {#if independentCountries.length > 0}
-	<h2>Independent Countries:</h2>
-	<ul>
-	  {#each independentCountries as country (country)}
-		<li>{country}</li>
-	  {/each}
-	</ul>
+  {#if selectedCountry}
+	<p>You selected: {selectedCountry}</p>
   {/if}
   
--- Aula04/dependency/src/CountryFlag.svelte
+++ Aula04/dependency/src/CountryFlag.svelte
@@ -4,14 +4,13 @@
     import { onMount } from 'svelte';
     import { createEventDispatcher } from 'svelte';
   
-    // Event dispatcher to communicate with the parent component
+    // Event dispatcher to communicate with parent component
     const dispatch = createEventDispatcher();
   
     // Data
     let countries = [];
     let selectedCountry = '';
     let countryFlagUrl = '';
-    let isIndependent = null;
   
     // Fetch a list of countries from the REST Countries API
     onMount(async () => {
@@ -24,22 +23,12 @@
     });
   
     // Update the selectedCountry and fetch the flag when a country is selected
-    async function handleCountrySelect() {
+    function handleCountrySelect() {
       if (selectedCountry) {
         const country = countries.find((c) => c.name === selectedCountry);
         if (country) {
           countryFlagUrl = country.flags[0];
-          // Fetch independence status
-          try {
-            const independenceResponse = await fetch(
-              `https://restcountries.com/v3.1/independent?status=true&name=${selectedCountry}`
-            );
-            const independenceData = await independenceResponse.json();
-            isIndependent = independenceData.status === 'independent';
-            dispatch('countrySelected', selectedCountry);
-          } catch (error) {
-            console.error('Error fetching independence status:', error);
-          }
+          dispatch('countrySelected', selectedCountry);
         }
       }
     }
@@ -56,11 +45,6 @@
     <div>
       <h3>Flag of {selectedCountry}</h3>
       <img src={countryFlagUrl} alt={`Flag of ${selectedCountry}`} />
-      {#if isIndependent === true}
-        <p>{selectedCountry} is independent.</p>
-      {:else if isIndependent === false}
-        <p>{selectedCountry} is not independent.</p>
-      {/if}
     </div>
   {/if}
   
